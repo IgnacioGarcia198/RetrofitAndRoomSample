4725b15cfe4d817c93f21de06464308b5115bc49
nacho <ignaciogarcia198@gmail.com>
2019-06-25 06:24:49 +0900

    Added the android test for the database. In this way we can be sure that it is working when saving or getting data.

---
+++ app/src/androidTest/java/com/example/retrofitfirstattempt/db/DBTest.java
@@ -0,0 +1,32 @@
+package com.example.retrofitfirstattempt.db;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Room;
+import android.support.test.InstrumentationRegistry;
+
+import com.example.retrofitfirstattempt.db.RetroPhotoDatabase;
+import com.example.retrofitfirstattempt.model.RetroPhoto;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+
+public class DBTest {
+    protected RetroPhotoDatabase db;
+
+    @Before
+    public void initDb() {
+        db = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
+                RetroPhotoDatabase.class).build();
+    }
+
+    @After
+    public void closeDb() {
+        db.close();
+    }
+
+
+}
